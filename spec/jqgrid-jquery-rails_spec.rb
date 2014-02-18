0977a5962591ead759d0a17ed331bc8c269df78c
Doc Walker <doc.walker@jameshardie.com>
2014-02-18 15:14:27 -0600

    Fix rubocop MethodCallParentheses offense

--- spec/jqgrid-jquery-rails_spec.rb
+++ spec/jqgrid-jquery-rails_spec.rb
@@ -5,7 +5,7 @@ describe The1kbgridCss::Rails do
   context 'VERSION constant' do
     it 'is a valid SemVer string (semver.org)' do
       version = The1kbgridCss::Rails::VERSION
-      expect { Semantic::Version.new version }.to_not raise_error()
+      expect { Semantic::Version.new version }.to_not raise_error
     end # it
   end # context
 end # describe
