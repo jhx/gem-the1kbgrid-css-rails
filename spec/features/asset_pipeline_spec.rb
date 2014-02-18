19da185d9c09237f97de7778b1cf1a6b764d6ba3
Doc Walker <doc.walker@jameshardie.com>
2014-02-18 15:26:07 -0600

    Fix rubocop TrailingWhitespace offenses

--- spec/features/asset_pipeline_spec.rb
+++ spec/features/asset_pipeline_spec.rb
@@ -4,7 +4,7 @@ feature 'Asset pipeline' do
   scenario 'Browser requests application stylesheet' do
     visit asset_path('application.css')
     # save_and_open_page  # for debugging (opens page in browser)
-    
+
     expect(page).to have_text('.grid_1')
   end # scenario
 end # feature
